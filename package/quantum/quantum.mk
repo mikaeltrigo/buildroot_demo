607ec84354f6d848d69c4e97ffa2e33fc1c10bbd
Pavol Kurina <pavol.kurina@gmail.com>
2019-08-23 18:26:43 +0200

    quantum: fixed to work with buildroot

--- package/quantum/quantum.mk
+++ package/quantum/quantum.mk
@@ -5,17 +5,24 @@
 ################################################################################
 
 QUANTUM_VERSION = 1.1
-QUANTUM_SITE =  $(patsubst %/,%,$(QUANTUM_PKGDIR))/Debug
+QUANTUM_SITE =  $(patsubst %/,%,$(QUANTUM_PKGDIR))
 QUANTUM_SITE_METHOD = local
+QUANTUM_BUILD_CONFIGURATION = Debug
+QUANTUM_PRE_BUILD_HOOKS += FIX_MAKEFILES
 
-#HELLOWORLD_INSTALL_STAGING = YES
+define FIX_MAKEFILES
+	find $(@D) -name "makefile" -o -name "*.mk" | xargs sed -i -e "s/arm-.*-gcc/\$$\(CC\)/g"
+	find $(@D) -name "makefile" -o -name "*.mk" | xargs sed -i -e "s:/home/mybbubuntu/workspace/testQuantum:$(@D):g"
+	find $(@D) -name "makefile" -o -name "*.mk" | xargs sed -i -e "s/arm-linux-gnueabi-size/size/g"
+endef
+
 
 define QUANTUM_BUILD_CMDS
-    $(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+    $(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/$(QUANTUM_BUILD_CONFIGURATION)
 endef
 
 define QUANTUM_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/testQuantum.elf $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/$(QUANTUM_BUILD_CONFIGURATION)/testQuantum.elf $(TARGET_DIR)/usr/bin/quantum
 endef
 
 $(eval $(generic-package))
