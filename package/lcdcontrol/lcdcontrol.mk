d0fd79beb857952d8ca04ca998d25dbc2f444774
Pavol Kurina <pavol.kurina@gmail.com>
2019-08-23 18:26:00 +0200

    lcdcontrol: fixed to work with buildroot

--- package/lcdcontrol/lcdcontrol.mk
+++ package/lcdcontrol/lcdcontrol.mk
@@ -5,17 +5,21 @@
 ################################################################################
 
 LCDCONTROL_VERSION = 1.1
-LCDCONTROL_SITE = $(patsubst %/,%,$(LCDCONTROL_PKGDIR))/Release
+LCDCONTROL_SITE = $(patsubst %/,%,$(LCDCONTROL_PKGDIR))
 LCDCONTROL_SITE_METHOD = local
+LCDCONTROL_PRE_BUILD_HOOKS += FIX_MAKEFILES
 
-#HELLOWORLD_INSTALL_STAGING = YES
+define FIX_MAKEFILES
+	find $(@D) -name "makefile" -o -name "*.mk" | xargs sed -i -e "s/arm-.*-gcc/\$$\(CC\)/g"
+	find $(@D) -name "makefile" -o -name "*.mk" | xargs sed -i -e "s:-I/usr/arm-linux-gnueabi/include::g"
+endef
 
 define LCDCONTROL_BUILD_CMDS
-    $(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+    $(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/Release
 endef
 
 define LCDCONTROL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/testLCD.elf $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/Release/testLCD.elf $(TARGET_DIR)/usr/bin/lcdcontrol
 endef
 
 $(eval $(generic-package))
