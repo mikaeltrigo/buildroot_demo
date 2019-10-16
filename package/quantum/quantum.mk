2e21240266b350746d628ef6596ff8c12abff426
Mikael Trigo <mikael.trigo@swisstiming.com>
2019-10-16 10:10:04 +0200

    update version of quantum and pwrt3lcdapp

--- package/quantum/quantum.mk
+++ package/quantum/quantum.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QUANTUM_VERSION = e0acdece5325a6c43a272a59f2bf0fd362a1a9e0
+QUANTUM_VERSION = 0.2
 QUANTUM_SITE_METHOD = git
 QUANTUM_SITE = 	https://github.com/mikaeltrigo/Quantum.git
 
