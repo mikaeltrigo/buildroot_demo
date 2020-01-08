fb3c5fb705e097ac6769fec02ba8a36c3c95cbed
Mikael Trigo <mikael.trigo@swisstiming.com>
2020-01-08 15:59:23 +0100

    add stable version FPGA and fsbl, update package

--- package/pwrt3_lua/pwrt3_lua.mk
+++ package/pwrt3_lua/pwrt3_lua.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PWRT3_LUA_VERSION = fde2ae5371b13042575ab9a76116f2f3a0256e60
+PWRT3_LUA_VERSION = 5682e374b8d765cbd04181a8a6248a98c73d0328
 PWRT3_LUA_SITE_METHOD = git
 PWRT3_LUA_SITE = https://github.com/mikaeltrigo/pwrt3_lua.git
 PWRT3_LUA_INSTALL_TARGET = YES
